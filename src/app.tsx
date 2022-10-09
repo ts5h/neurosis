ac3d45ba440966b751513792f3424dc4f5eca5ca
at4 <at4@jc4.so-net.ne.jp>
2022-10-09 19:21:45 +0200

    Update files

--- src/app.tsx
+++ src/app.tsx
@@ -1,5 +1,5 @@
-import * as React from 'react';
-import * as ReactDOM from 'react-dom';
+import * as React from "react";
+import * as ReactDOM from "react-dom";
 
 function render() {
   ReactDOM.render(<h2>Hello from React!</h2>, document.body);
--- webpack.renderer.config.js
+++ webpack.renderer.config.js
@@ -1,10 +1,20 @@
-const rules = require('./webpack.rules');
-const plugins = require('./webpack.plugins');
+const rules = require("./webpack.rules");
+const plugins = require("./webpack.plugins");
 
-rules.push({
-  test: /\.css$/,
-  use: [{ loader: 'style-loader' }, { loader: 'css-loader' }],
-});
+rules.push(
+  {
+    test: /\.css$/,
+    use: [{ loader: "style-loader" }, { loader: "css-loader" }],
+  },
+  {
+    test: /\.s[ac]ss$/,
+    use: [
+      { loader: "style-loader" },
+      { loader: "css-loader" },
+      { loader: "sass-loader" },
+    ],
+  }
+);
 
 module.exports = {
   module: {
@@ -12,6 +22,6 @@ module.exports = {
   },
   plugins: plugins,
   resolve: {
-    extensions: ['.js', '.ts', '.jsx', '.tsx', '.css'],
+    extensions: [".js", ".ts", ".jsx", ".tsx", ".css", ".sass", ".scss"],
   },
 };
